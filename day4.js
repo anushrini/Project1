2b892176be937522938935de42fb0330354ad31a
anushrini <143867036+anushrini@users.noreply.github.com>
2024-08-08 15:18:57 +0530

    Add files via upload

---
+++ day4.js
@@ -0,0 +1,72 @@
+//function abc()
+//{
+  //      console.log("Hello");
+//}
+//abc();
+
+
+//---->ARROW FUNCTIONS
+//let f=(a)=>
+//{
+//    return a*a;
+
+//}
+//let cub=(a)=>a**3;
+//console.log(cub(4));
+
+
+
+//..........>TEMPLATE STRING
+//let k=45;
+//let j=23;
+// let s="ab\nc";//s=abc or ab\nc then outputs are diff
+// let q= s + j + k;
+// let d="hello";
+// console.log(q);
+
+
+
+// ..............>ARRAYS
+let arr=[true,45,4.1,undefined,null,"string","qwert"];
+// console.log(arr);
+
+
+
+// for(let i=0;i<arr.length;i++)
+// {
+//   console.log(arr[i]);
+// }
+
+// arr.forEach(
+//   function(ele,ind,arr)
+//   {
+//       console.log(ele,ind,arr);
+//   }
+// )
+
+
+
+
+
+//..........Array methods.........
+// arr.push(10);
+// arr.push(20);
+// arr.push(30);
+// console.log(arr);
+// arr.shift();
+// arr.unshift(45);
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+
+
+
+
+// JSON ......>JS object notation
+let k={
+  45:"value",
+  hello:4.45656,
+  true:98
+ arr:[4,5,true]
+};
+console.log(k);
